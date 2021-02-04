eb1b9009db05e8d65d4aaf99217c68886baeeac4
pc <pc@14.32.199.14>
2021-02-04 18:54:45 +0900

    20210204 - 송준원 예매버튼 건들지 말아주세요 ㅎ

--- src/run/member/payment/Paymentrun.java
+++ src/run/member/payment/Paymentrun.java
@@ -79,7 +79,7 @@ public class Paymentrun extends JPanel {
 		this.mf = mf;
 		this.setSize(1200, 801);
 		this.Paymentrun = this;
-		
+		System.out.println(nickName);
 		try {
 			img = ImageIO.read(new File("img/movie.jpg"));
 		} catch (IOException e) {
@@ -149,17 +149,20 @@ public class Paymentrun extends JPanel {
 		hight += 80;
 		JButton[] btn2 = new JButton[MovieList.size()];
 		for (int i = 0; i < areaList.size(); i++) {
+
 			String List = (String) areaList.get(i);
 			btn2[i]=new JButton(List);//버튼 초기화
 			btn2[i].setBounds(width,hight, 150, 30); //버튼 크기,위치 지정
 			width += 150;
 			btn2[i].setBackground(Color.WHITE);
 			Paymentrun.add(btn2[i]);
+			
 		}
 		/* 날짜 선택 */
 		width = 240;
 		hight += 80;
 		JButton[] btn3 = new JButton[dateList.size()];
+		
 		for (int i = 0; i < dateList.size(); i++) {
 			String List = (String) dateList.get(i);
 			btn3[i]=new JButton(List);//버튼 초기화
@@ -251,6 +254,7 @@ public class Paymentrun extends JPanel {
 		btn1[0].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn1[0].equals("선택완료"))) {
 				movie = (String) MovieList.get(0);
 				movidPrice = dao.moviePrice(0);
 				areaList2 = dao.MovieListArea((String) MovieList.get(0));
@@ -258,12 +262,19 @@ public class Paymentrun extends JPanel {
 				String List2 = (String) areaList2.get(i);
 				btn2[i].setText(List2);
 				movieNumber = 0;
+				btn1[0].setText("선택완료");
+				btn1[1].setText("선택완료");
+				btn1[2].setText("선택완료");
+				btn1[3].setText("선택완료");
+				btn1[4].setText("선택완료");
+				}
 				}
 			}
 		});
 		btn1[1].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn1[0].equals("선택완료"))) {
 				movie = (String) MovieList.get(1);
 				movidPrice = dao.moviePrice(1);
 				areaList2 = dao.MovieListArea((String) MovieList.get(1));
@@ -271,12 +282,19 @@ public class Paymentrun extends JPanel {
 				String List2 = (String) areaList2.get(i);
 				btn2[i].setText(List2);
 				movieNumber = 1;
+				btn1[0].setText("선택완료");
+				btn1[1].setText("선택완료");
+				btn1[2].setText("선택완료");
+				btn1[3].setText("선택완료");
+				btn1[4].setText("선택완료");
+				}
 				}
 			}
 		});
 		btn1[2].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn1[0].equals("선택완료"))) {
 				movie = (String) MovieList.get(2);
 				movidPrice = dao.moviePrice(2);
 				areaList2 = dao.MovieListArea((String) MovieList.get(2));
@@ -284,12 +302,19 @@ public class Paymentrun extends JPanel {
 				String List2 = (String) areaList2.get(i);
 				btn2[i].setText(List2);
 				movieNumber = 2;
+				btn1[0].setText("선택완료");
+				btn1[1].setText("선택완료");
+				btn1[2].setText("선택완료");
+				btn1[3].setText("선택완료");
+				btn1[4].setText("선택완료");
+				}
 				}
 			}
 		});
 		btn1[3].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn1[0].equals("선택완료"))) {
 				movie = (String) MovieList.get(3);
 				movidPrice = dao.moviePrice(3);
 				areaList2 = dao.MovieListArea((String) MovieList.get(3));
@@ -297,12 +322,19 @@ public class Paymentrun extends JPanel {
 				String List2 = (String) areaList2.get(i);
 				btn2[i].setText(List2);
 				movieNumber = 3;
+				btn1[0].setText("선택완료");
+				btn1[1].setText("선택완료");
+				btn1[2].setText("선택완료");
+				btn1[3].setText("선택완료");
+				btn1[4].setText("선택완료");
+				}
 				}
 			}
 		});
 		btn1[4].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn1[0].equals("선택완료"))) {
 				movie = (String) MovieList.get(4);
 				movidPrice = dao.moviePrice(4);
 				areaList2 = dao.MovieListArea((String) MovieList.get(4));
@@ -310,7 +342,12 @@ public class Paymentrun extends JPanel {
 				String List2 = (String) areaList2.get(i);
 				btn2[i].setText(List2);
 				movieNumber = 4;
-				System.out.println(movie);
+				btn1[0].setText("선택완료");
+				btn1[1].setText("선택완료");
+				btn1[2].setText("선택완료");
+				btn1[3].setText("선택완료");
+				btn1[4].setText("선택완료");
+				}
 				}
 				
 			}
@@ -321,61 +358,95 @@ public class Paymentrun extends JPanel {
 		btn2[0].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn2[0].equals("선택완료"))) {
 				movidArea = (String) areaList2.get(0);
 				dateList = dao.movieDate((String) MovieList.get(movieNumber), (String) areaList.get(0));
 				for (int i = 0; i < dateList.size(); i++) {
 				String List = (String) dateList.get(i);
 				btn3[i].setText(List); 
 				movieNumber2 = 0;
+				btn2[0].setText("선택완료");
+				btn2[1].setText("선택완료");
+				btn2[2].setText("선택완료");
+				btn2[3].setText("선택완료");
+				btn2[4].setText("선택완료");
+				}
 				}
 			}
 		});
 		btn2[1].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn2[0].equals("선택완료"))) {
 				movidArea = (String) areaList2.get(1);
 				dateList = dao.movieDate((String) MovieList.get(movieNumber), (String) areaList.get(1));
 				for (int i = 0; i < dateList.size(); i++) {
 				String List = (String) dateList.get(i);
 				btn3[i].setText(List); 
 				movieNumber2 = 1;
+				btn2[0].setText("선택완료");
+				btn2[1].setText("선택완료");
+				btn2[2].setText("선택완료");
+				btn2[3].setText("선택완료");
+				btn2[4].setText("선택완료");
+				}
 				}
 			}
 		});
 		btn2[2].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn2[0].equals("선택완료"))) {
 				movidArea = (String) areaList2.get(2);
 				dateList = dao.movieDate((String) MovieList.get(movieNumber), (String) areaList.get(2));
 				for (int i = 0; i < dateList.size(); i++) {
 				String List = (String) dateList.get(i);
 				btn3[i].setText(List); 
 				movieNumber2 = 2;
+				btn2[0].setText("선택완료");
+				btn2[1].setText("선택완료");
+				btn2[2].setText("선택완료");
+				btn2[3].setText("선택완료");
+				btn2[4].setText("선택완료");
+				}
 				}
 			}
 		});
 		btn2[3].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn2[0].equals("선택완료"))) {
 				movidArea = (String) areaList2.get(3);
 				dateList = dao.movieDate((String) MovieList.get(movieNumber), (String) areaList.get(3));
 				for (int i = 0; i < dateList.size(); i++) {
 				String List = (String) dateList.get(i);
 				btn3[i].setText(List);
 				movieNumber2 = 3;
+				btn2[0].setText("선택완료");
+				btn2[1].setText("선택완료");
+				btn2[2].setText("선택완료");
+				btn2[3].setText("선택완료");
+				btn2[4].setText("선택완료");
+				}
 				}
 			}
 		});
 		btn2[4].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn2[0].equals("선택완료"))) {
 				movidArea = (String) areaList2.get(4);
 				dateList = dao.movieDate((String) MovieList.get(movieNumber), (String) areaList.get(4));
 				for (int i = 0; i < dateList.size(); i++) {
 				String List = (String) dateList.get(i);
 				btn3[i].setText(List); 
 				movieNumber2 = 4;
-				System.out.println(movidArea);
+				btn2[0].setText("선택완료");
+				btn2[1].setText("선택완료");
+				btn2[2].setText("선택완료");
+				btn2[3].setText("선택완료");
+				btn2[4].setText("선택완료");
+				}
 				}
 				
 			}
@@ -385,54 +456,83 @@ public class Paymentrun extends JPanel {
 		btn3[0].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn3[0].equals("선택완료"))) {
 				date = (String) dateList.get(0);
 				timeList = dao.movieTime((String) MovieList.get(movieNumber), (String) areaList.get(movieNumber1), (String) dateList.get(0));
 				for (int i = 0; i < timeList.size(); i++) {
 				String List = (String) timeList.get(i);
 				btn4[i].setText(List); 
 				movieNumber2 = 0;
+				btn3[0].setText("선택완료");
+				btn3[1].setText("선택완료");
+				btn3[2].setText("선택완료");
+				btn3[3].setText("선택완료");
+				btn3[4].setText("선택완료");
+				}
 				}
 			}
 		});
 		btn3[1].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn3[0].equals("선택완료"))) {
 				date = (String) dateList.get(1);
 				timeList = dao.movieTime((String) MovieList.get(movieNumber), (String) areaList.get(movieNumber1), (String) dateList.get(1));
 				for (int i = 0; i < timeList.size(); i++) {
 				String List = (String) timeList.get(i);
 				btn4[i].setText(List); 
 				movieNumber2 = 1;
+				btn3[0].setText("선택완료");
+				btn3[1].setText("선택완료");
+				btn3[2].setText("선택완료");
+				btn3[3].setText("선택완료");
+				btn3[4].setText("선택완료");
+				}
 				}
 			}
 		});
 		btn3[2].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn3[0].equals("선택완료"))) {
 				date = (String) dateList.get(2);
 				timeList = dao.movieTime((String) MovieList.get(movieNumber), (String) areaList.get(movieNumber1), (String) dateList.get(2));
 				for (int i = 0; i < timeList.size(); i++) {
 				String List = (String) timeList.get(i);
 				btn4[i].setText(List); 
 				movieNumber2 = 2;
+				btn3[0].setText("선택완료");
+				btn3[1].setText("선택완료");
+				btn3[2].setText("선택완료");
+				btn3[3].setText("선택완료");
+				btn3[4].setText("선택완료");
+				}
 				}
 			}
 		});
 		btn3[3].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn3[0].equals("선택완료"))) {
 				date = (String) dateList.get(3);
 				timeList = dao.movieTime((String) MovieList.get(movieNumber), (String) areaList.get(movieNumber1), (String) dateList.get(3));
 				for (int i = 0; i < timeList.size(); i++) {
 				String List = (String) timeList.get(i);
 				btn4[i].setText(List); 
 				movieNumber2 = 3;
+				btn3[0].setText("선택완료");
+				btn3[1].setText("선택완료");
+				btn3[2].setText("선택완료");
+				btn3[3].setText("선택완료");
+				btn3[4].setText("선택완료");
+				}
 				}
 			}
 		});
 		btn3[4].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn3[0].equals("선택완료"))) {
 				date = (String) dateList.get(4);
 				timeList = dao.movieTime((String) MovieList.get(movieNumber), (String) areaList.get(movieNumber1), (String) dateList.get(4));
 				for (int i = 0; i < timeList.size(); i++) {
@@ -440,6 +540,12 @@ public class Paymentrun extends JPanel {
 				btn4[i].setText(List); 
 				movieNumber2 = 4;
 				System.out.println(date);
+				btn3[0].setText("선택완료");
+				btn3[1].setText("선택완료");
+				btn3[2].setText("선택완료");
+				btn3[3].setText("선택완료");
+				btn3[4].setText("선택완료");
+				}
 				}
 			}
 		});
@@ -448,61 +554,95 @@ public class Paymentrun extends JPanel {
 		btn4[0].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn4[0].equals("선택완료"))) {
 				time = (String) timeList.get(0);
 				seatList = dao.movieSeat((String) MovieList.get(movieNumber), (String) areaList.get(movieNumber1), 
 										 (String) dateList.get(movieNumber2),(String) timeList.get(0));
 				for (int i = 0; i < seatList.size()-1; i++) {
 					String List = (String) seatList.get(i+1);
-				btn5[i].setText(List); 
+				btn5[i].setText(List);
+				btn4[0].setText("선택완료");
+				btn4[1].setText("선택완료");
+				btn4[2].setText("선택완료");
+				btn4[3].setText("선택완료");
+				btn4[4].setText("선택완료");
+				}
 				}
 			}
 		});
 		btn4[1].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn4[0].equals("선택완료"))) {
 				time = (String) timeList.get(1);
 				seatList = dao.movieSeat((String) MovieList.get(movieNumber), (String) areaList.get(movieNumber1), 
 										 (String) dateList.get(movieNumber2),(String) timeList.get(1));
 				for (int i = 0; i < seatList.size()-1; i++) {
 					String List = (String) seatList.get(i+1);
 				btn5[i].setText(List); 
+				btn4[0].setText("선택완료");
+				btn4[1].setText("선택완료");
+				btn4[2].setText("선택완료");
+				btn4[3].setText("선택완료");
+				btn4[4].setText("선택완료");
+				}
 				}
 			}
 		});
 		btn4[2].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn4[0].equals("선택완료"))) {
 				time = (String) timeList.get(2);
 				seatList = dao.movieSeat((String) MovieList.get(movieNumber), (String) areaList.get(movieNumber1), 
 										 (String) dateList.get(movieNumber2),(String) timeList.get(2));
 				for (int i = 0; i < seatList.size()-1; i++) {
 				String List = (String) seatList.get(i+1);
 				btn5[i].setText(List); 
+				btn4[0].setText("선택완료");
+				btn4[1].setText("선택완료");
+				btn4[2].setText("선택완료");
+				btn4[3].setText("선택완료");
+				btn4[4].setText("선택완료");
+				}
 				}
 			}
 		});
 		btn4[3].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn4[0].equals("선택완료"))) {
 				time = (String) timeList.get(3);
 				seatList = dao.movieSeat((String) MovieList.get(movieNumber), (String) areaList.get(movieNumber1), 
 										 (String) dateList.get(movieNumber2),(String) timeList.get(3));
 				for (int i = 0; i < seatList.size()-1; i++) {
 					String List = (String) seatList.get(i+1);
 				btn5[i].setText(List);  
+				btn4[0].setText("선택완료");
+				btn4[1].setText("선택완료");
+				btn4[2].setText("선택완료");
+				btn4[3].setText("선택완료");
+				btn4[4].setText("선택완료");
+				}
 				}
 			}
 		});
 		btn4[4].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if(!(btn4[0].equals("선택완료"))) {
 				time = (String) timeList.get(4);
 				seatList = dao.movieSeat((String) MovieList.get(movieNumber), (String) areaList.get(movieNumber1), 
 										 (String) dateList.get(movieNumber2),(String) timeList.get(4));
 				for (int i = 0; i < seatList.size()-1; i++) {
 					String List = (String) seatList.get(i+1);
 				btn5[i].setText(List); 
-				System.out.println(time);
+				btn4[0].setText("선택완료");
+				btn4[1].setText("선택완료");
+				btn4[2].setText("선택완료");
+				btn4[3].setText("선택완료");
+				btn4[4].setText("선택완료");
+				}
 				}
 			}
 		});
@@ -522,13 +662,13 @@ public class Paymentrun extends JPanel {
 		btn5[1].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(btn5[2].equals("0")) {
+				if(btn5[1].equals("0")) {
 				}else{
 					seat += (String) seatList.get(2);
 					ticketPrice += movidPrice;
 					viewer += 1;
 				}
-				btn5[2].setText("0");
+				btn5[1].setText("0");
 			}
 		});
 		btn5[2].addActionListener(new ActionListener() {
@@ -640,6 +780,7 @@ public class Paymentrun extends JPanel {
 		card1.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				String text = "선택완료";
 				cardName = "카카오톡";
 				cardDiscount = 3.0;
 				pay = movidPrice + productPrice;
@@ -648,6 +789,8 @@ public class Paymentrun extends JPanel {
 				SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 				Calendar time = Calendar.getInstance();
 				toDay = format1.format(time.getTime())+"";
+
+
 			}
 		});
 		card2.addActionListener(new ActionListener() {
@@ -694,7 +837,7 @@ public class Paymentrun extends JPanel {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.out.println(movie+ " "+movidArea+ " "+date+ " "+ time + " "+seat+ " "+ticketPrice+ " "+cardName+ " "+cardDiscount+ " "+pay+ " "+toDay+" "+product+" "+productPrice);
-//				dao.print(nickName, movie, movidArea, date, time, seat, viewer+"", ticketPrice, product, productPrice+"", cardName, cardDiscount+"", pay, toDay);
+				dao.print(nickName, movie, movidArea, date, time, seat, viewer+"", ticketPrice, product, productPrice+"", cardName, cardDiscount+"", pay, toDay);
 				ChangePanel.changePanel(mf, Paymentrun, new Main(mf, memberInfo));
 			}
 		});
